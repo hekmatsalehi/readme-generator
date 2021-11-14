8e2aa98722e85ea0302a1b1ff9159e82bef40eb9
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2021-11-13 22:39:35 -0800

    Completed the README generator application

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-
-
-
 // Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
-
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // Create an array of questions for user input
 const questions = [
@@ -37,7 +18,7 @@ const questions = [
     {
         type: 'input',
         message: 'Provide the project installation instructions?',
-        name: 'installition',
+        name: 'installation',
     },
     {
         type: 'input',
@@ -58,17 +39,12 @@ const questions = [
         type: 'list',
         message: 'Choose the project license?',
         name: 'license',
-        choices: [ "MIT License", "GNU GPLv3", "Apache License 2.0", "Mozilla Public License 2.0", ],
+        choices: [ "MITLicense", "GNUGPLv3", "ApacheLicense2.0", "MozillaPublicLicense2.0", "No License"],
     },
     {
         type: 'input',
         message: 'Enter your gitHub username?',
         name: 'gitHubUserName',// Goes in Questions section
-    },
-    {
-        type: 'input',
-        message: 'Enter your gitHub url?',
-        name: 'gitHubUrl',//Goes in Questions section
     },
     {
         type: 'input',
@@ -80,16 +56,13 @@ const questions = [
 
 inquirer
     .prompt(questions)
-    .then((response) => {
-        console.log(response)
+    .then((answers) => {
+        const markdown = generateMarkdown(answers);
+        fs.writeFile('README.md', markdown, (err) =>{
+            if (err)
+            console.log(err);
+            else {
+                console.log("Written successfully")
+            }
+        })
     })
-    
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,70 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if(license === "No License"){
+      return''
+  } else {
+      return`![badge](https://img.shields.io/badge/license-${license}-brightgreen)`
+  }
 }
+
+// Function takes care of License section of the README
+function renderLicense(license) {
+  if(license === "No License"){
+      return`This Application does not have any license`
+  } else {
+      return`This Application is covered under ${license}`
+  }
+}
+
+// function that returns the license link
+function renderLicenseLink(license) {
+  if (license === 'MITLicense'){
+    return `For more information about the License visit [MIT License Page](https://choosealicense.com/licenses/mit/)`
+  }
+  if (license === 'GNUGPLv3'){
+    return `For more information about the License visit [GNU GPLv3 Page](https://choosealicense.com/licenses/gpl-3.0/)`
+  }
+  if (license === 'ApacheLicense2.0'){
+    return `For more information about the License visit [Apache License2.0 Page](https://choosealicense.com/licenses/apache-2.0/)`
+  }
+  if (license === 'MozillaPublicLicense2.0'){
+    return `For more information about the License visit [Mozilla Public License 2.0 Page](https://choosealicense.com/licenses/mpl-2.0/)`
+  }
+  else 
+  if (license === "No License")
+  return ''
+}
+
+
+// Generate markdown for README
+const generateMarkdown = ({title, description, installation, usage, contributing, tests,
+  license, gitHubUserName, email}) =>
+  `# ${title}
+## Description
+${description}
+
+${renderLicenseBadge(license)}
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+## Installation
+${installation}
+## Usage
+${usage}
+## Contributing
+${contributing}
+## Tests
+${tests}
+## License
+${renderLicense(license)}
+
+${renderLicenseLink(license)}
+## Questions
+For further questions please contact me at:\n
+GitHub: [https://github.com/${gitHubUserName}](https://github.com/${gitHubUserName})\n
+Email: [${email}](mailto:${email})`;
 
 module.exports = generateMarkdown;
